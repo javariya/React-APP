67bfb088fa36a94d1a7bad9fb82dc6e39c2ddb6e
javariya <javariyaaamir@ymail.com>
2020-06-01 23:33:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World from Javariya</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am going to learn React!
         </p>
         <a
           className="App-link"
